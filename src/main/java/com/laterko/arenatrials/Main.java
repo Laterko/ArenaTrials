84f8d773a96ef5d374c22c1fa5bc92895a6c9bec
Lateralny <piotrjgladysz@gmail.com>
2022-03-15 21:40:50 +0100

    first commit

---
+++ src/main/java/com/laterko/arenatrials/Main.java
@@ -0,0 +1,17 @@
+package com.laterko.arenatrials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("YES! Plugin has started");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
